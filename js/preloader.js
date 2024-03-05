7a1e7d3e2441496c71b01320647cb047e095d09c
accalisha <t.alisher.1997@gmail.com>
2024-03-05 16:51:39 +0500

    Anime
    Anime

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,11 @@
-const preloader = document.querySelector(' .preloder')
 
-preloader.classList.add('active')
+const preloader = () => {
+    const preloader = document.querySelector(' .preloder')
+    preloader.classList.add('active')
 
-setTimeout(() => {
-    preloader.classList.remove('active')
-}, 500)
+    setTimeout(() => {
+        preloader.classList.remove('active')
+    }, 500)
+}
+
+preloader()
